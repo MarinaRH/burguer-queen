46de99608434642f35e9610ba74cc14cf89f036e
MarinaRh <l_marina1717@hotmail.com>
2018-03-07 20:01:34 -0500

    Agregando Bootstrap4

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.gif';
 import './Header.css';
 
 class Header extends Component {
@@ -8,6 +8,17 @@ class Header extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Menu</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Combos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Promociones</a>
+            </li>
+          </ul>
         </header>
       </div>
     );
